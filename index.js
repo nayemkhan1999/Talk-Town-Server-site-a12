588caa4df15111472c2612832a8e9bfea955344b
nayemkhan1999 <1999workmail@gmail.com>
2024-06-04 20:33:23 +0600

    Added a delete opereaton apply

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ async function run() {
       res.send(result);
     });
 
+    // My post theke Post Delete kora
+    app.delete("/allPost/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await forumCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
