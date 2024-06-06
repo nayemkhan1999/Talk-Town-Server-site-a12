d97d0b44a509fcfeda7add8bbb584c63550f4aaf
nayemkhan1999 <1999workmail@gmail.com>
2024-06-06 15:50:34 +0600

    User updated role play and update method apply

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,7 +14,12 @@ app.use(
 );
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+  Timestamp,
+} = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l4sutcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +37,29 @@ async function run() {
     const forumCollection = client.db("TalkTime").collection("posts");
     const userCollection = client.db("TalkTime").collection("users");
 
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "10h",
+      });
+      res.send({ token });
+    });
+    // MiddleWares VeriFy TOKEN
+    // const verifyToken = (req, res, next) => {
+    //   console.log("inside verify token", req.headers.authorization);
+    //   if (!req.headers.authorization) {
+    //     return res.status(401).send({ message: "forbidden access" });
+    //   }
+    //   const token = req.headers.authorization.split(" ")[1];
+    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //     if (err) {
+    //       return res.status(401).send({ message: "forbidden access" });
+    //     }
+    //     req.decoded = decoded;
+    //     next();
+    //   });
+    // };
     // =================Making Role Admin & User===========================
     app.get("/Role/:email", async (req, res) => {
       const email = req.params.email;
@@ -53,6 +82,18 @@ async function run() {
     // Admin can see all user details
     app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    //===================Update a user role============
+    app.patch("/userRoleChange/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = req.body;
+      const updateDoc = {
+        $set: { role: user.changeRole },
+      };
+      const result = await userCollection.updateOne(query, updateDoc);
       res.send(result);
     });
 
